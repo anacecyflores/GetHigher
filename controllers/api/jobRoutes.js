5a4ca9b2bde71f781a5e91bb730d6da669701bd8
mbenson025 <mbenson025@gmail.com>
2022-07-12 09:16:37 -0500

    more variables change to link jobs content to page

--- controllers/api/jobRoutes.js
+++ controllers/api/jobRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Job } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newJob = await Job.create({
       ...req.body,
-      user_id: req.session.user_id,
+      title: req.session.title,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newJob);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const JobData = await Job.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        title: req.session.title,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!JobData) {
+      res.status(404).json({ message: 'No job found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(JobData);
   } catch (err) {
     res.status(500).json(err);
   }
