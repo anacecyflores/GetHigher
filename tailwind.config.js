164ba575f4561f7826dd6503df72792b5c116f8d
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-07-12 14:54:29 -0500

    downloaded tailwindcss dependency, created tailwind.config.js file, created src folder, and input.css inside to hold the paths to the handlebars which is our content. When you run tailwind to scan for classes it works.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        '/views/layouts/main.handlebars',
+        '/views/calendar.handlebars',
+        '/views/homepage.handlebars',
+        '/views/jobs.handlebars',
+        '/views/login.handlebars',
+        '/views/profile.handlebars',
+        '/views/project.handlebars'
+      ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
