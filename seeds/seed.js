736af5dfc7225860cb9082302ef75a6b3f29c812
mbenson025 <mbenson025@gmail.com>
2022-07-12 15:14:29 -0500

    seed file quick fix

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Project, Jobs } = require('../models');
+const { User, Career } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+// const projectData = require('./projectData.json');
 
 const jobsData = require('./jobsData.json');
 
@@ -14,15 +14,15 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  // for (const project of projectData) {
+  //   await Project.create({
+  //     ...project,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   for (const jobs of jobsData) {
-    await Jobs.create({
+    await Career.create({
       ...jobs,
     });
   }
