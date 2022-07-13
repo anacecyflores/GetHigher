5ee9716e0afad700b26d20b6bf5128af42fb4e15
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-07-12 19:45:48 -0500

    added gray color to mainhandlebars. Tailwind works now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        '/views/layouts/main.handlebars',
-        '/views/calendar.handlebars',
-        '/views/homepage.handlebars',
-        '/views/jobs.handlebars',
-        '/views/login.handlebars',
-        '/views/profile.handlebars',
-        '/views/project.handlebars'
-      ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    './views/layouts/main.handlebars',
+    './js/logout.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
