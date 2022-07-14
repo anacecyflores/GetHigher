5c1f82685ba1187539a5ff0239577f5baaf2eff7
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-07-14 10:36:04 -0500

    added styling for landing page, and ogin/signup page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './views/layouts/main.handlebars',
-    './js/logout.js',
-    './views/login.handlebars',
-    './js/login.js',
-
+    './views/**/*.handlebars',
+    './views/*.handlebars',
   ],
   theme: {
-    colors: {
-  
-    },
     extend: {},
   },
   plugins: [require('@tailwindcss/forms'),],
