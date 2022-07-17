677e7e12e791f4e2ca633059143ae24a2acf1c37
Cecy Flores <anacecyflores1@gmail.com>
2022-07-17 10:25:23 -0500

    LOG IN WORKS '

--- public/js/login.js
+++ public/js/login.js
@@ -14,8 +14,8 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -37,7 +37,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
