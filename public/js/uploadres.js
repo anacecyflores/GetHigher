fcf7032f54ec6e70ef107020402371298140663f
Cristian Vargas <cristian.v0223@gmail.com>
2022-07-21 13:52:31 -0500

    added media query for map in saved jobs so mobile site looks better with map

--- public/js/uploadres.js
+++ public/js/uploadres.js
@@ -1,17 +1,19 @@
-const formImageHandler = async function (event) {
+//Upload Resume Function
+const formResumeHandler = async function (event) {
     event.preventDefault();
     console.log('---------------')
     var fd = new FormData();
     const resume = document.querySelector('#file-upload').files[0]
     fd.append("resume", resume);
 
-    // const response = await fetch('api/users/upload', {
-    //     method: 'POST',
-    //     body: fd
-    // })
-    // if (response.ok) {
-    //     document.location.replace('/dashboard/profile');
-    // }
+    //route needs to be adjusted, below is copied code from https://nanny-poppins.herokuapp.com
+    const response = await fetch('api/users/upload', {
+        method: 'POST',
+        body: fd
+    })
+    if (response.ok) {
+        document.location.replace('/dashboard/profile');
+    }
 }
 document.querySelector('#submitRes')
-    .addEventListener('submit', formImageHandler)
+    .addEventListener('submit', formResumeHandler)
