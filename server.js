76173109f138d6495137dcd8138a068157500fb4
Cristian Vargas <cristian.v0223@gmail.com>
2022-07-21 09:58:44 -0500

    un linked js files from main in efforts to revert heroku error

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const { checkFileUploadType } = require('./aws');
-const multer = require('multer');
-const upload = multer({
-  //5MB file upload limit
-  limits:{
-    fileSize: 5000000,
-  // checks the file type 
-  fileFilter: function (req,file,cb){
-      checkFileUploadType(file, cb)
-  }
-  }
-})
+// const { checkFileUploadType } = require('./aws');
+// const multer = require('multer');
+// const upload = multer({
+//   //5MB file upload limit
+//   limits:{
+//     fileSize: 5000000,
+//   // checks the file type 
+//   fileFilter: function (req,file,cb){
+//       checkFileUploadType(file, cb)
+//   }
+//   }
+// })
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 //Multer Middleware for resume upload
-app.use(upload.single('resume'))
+// app.use(upload.single('resume'))
 
 app.use(routes);
 
